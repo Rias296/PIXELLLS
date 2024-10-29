fec75db33c0a1c1129693b509f59702dcd139b2c
Rias296 <tanamaefrain@gmail.com>
2024-10-29 16:25:44 +1100

    fix issues with client class

--- src/Tools/Multiplayer/client.cs
+++ src/Tools/Multiplayer/client.cs
@@ -3,23 +3,54 @@ using Godot;
 
 public class Client :Node
 {
-
+	private NetworkedMultiplayerENet peer;
 
 	public override void _Ready()
-	{
-		ConnectToServer(Multiplayer_Constants.IP_ADDRESS,Multiplayer_Constants.PORT);
-		GD.Print("Connected to server");
+	{	
+		ConnectToServer();
 	}
 
-	public void ConnectToServer(string ip, int port){
-		var peer = new NetworkedMultiplayerENet();
-		peer.CreateClient(ip, port);
-		GetTree().NetworkPeer = peer;
-		GetTree().SetMeta(Multiplayer_Constants.NETWORK_PEER,peer);
+	private void ConnectToServer()
+	{
+		peer = new NetworkedMultiplayerENet();
+		
+		Error connectionResult = peer.CreateClient(Multiplayer_Constants.IP_ADDRESS, Multiplayer_Constants.PORT);
+		if (connectionResult != Error.Ok)
+		{
+			GD.PrintErr($"Unable to connect to the server on port {Multiplayer_Constants.PORT}. Error: {connectionResult}");
+			return;
+		}
 
-		 // Connect to MultiplayerAPI signals for network connection and disconnection
-		GetTree().IsNetworkServer();
-		GetTree().NetworkPeer = null;
+		// Set the network peer directly on the SceneTree
+		GetTree().NetworkPeer = peer;
+		
+		// Connect to the server connection signal
+		
+		if (GetTree().Connect("connected_to_server", this, nameof(OnConnectedToServer)) == Error.Ok){
+			GD.Print("Successfully connected");
+			GetTree().ChangeScene("res://Scenes/Main Menu/LoginScreen.tscn");
+		}else{
+			GD.Print("Did not connect");
+		}
+
+		GD.Print("Client started and attempting to connect...");
+	}
+
+	private void OnConnectedToServer()
+	{
+		GD.Print("Successfully connected to server!");
+		// Activate login screen or change scenes here
+		
+	}
+
+	 public override void _ExitTree()
+	{
+		if (peer != null)
+		{
+			peer.CloseConnection();
+			peer = null;
+			GD.Print("Client did not connect to server");
+		}
 	}
 
 
